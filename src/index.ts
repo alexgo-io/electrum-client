94d755d404424142f64728db610911e9687e3939
xin <lighthx@gmail.com>
2025-04-01 16:25:11 +0800

    feat: remove esmodule type

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import type {
   PersistencePolicy,
   Protocol,
 } from "./types";
-import type { TransactionVerbose } from "./types/transactionVerbose.js";
+import type { TransactionVerbose } from "./types/transactionVerbose";
 import type { MempoolTransaction } from "./types/mempoolTransaction";
 
 export class ElectrumClient extends Client {
